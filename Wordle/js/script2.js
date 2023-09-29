cbc11c9de1cf32dbf853006bcdeb8b6912c6dfc4
nickfeltham33 <74492655+nickfeltham33@users.noreply.github.com>
2023-09-29 14:45:32 +0100

    my attempt at the commit function

--- Wordle/js/script2.js
+++ Wordle/js/script2.js
@@ -1,4 +1,5 @@
 const letters = document.querySelectorAll('.letter');
+const loadingDiv = document.querySelector('.loading');
 
 let currentGuess = '';
 const ANSWER_LENGTH = 5;
@@ -7,14 +8,20 @@ let currentRow = 0;
 
 
 async function init() {
-    const res = await fetch("https://words.dev-apis.com/word-of-the-day")
-    console.log(res);
+
+    const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+    const resObj = await res.json();
+    const word = resObj.word.toUpperCase();
+    const wordArray = word.split("");
+
+    isLoading = false;
+    setLoading(isLoading);
+
     document.addEventListener("keydown", function handleKeyPress (event) {
         const action = event.key;
-        console.log(action);
 
         if (action === 'Enter') {
-            commit();
+            commit(wordArray);
         } else if (action === 'Backspace') {
             backspace();
         } else if (isLetter(action)) {
@@ -29,17 +36,28 @@ const isLetter = (letter) => {
     return /^[a-zA-Z]$/.test(letter);
 }
 
-const commit = () => {
-    console.log("commit");
+const commit = (wordArray) => {
     if (currentGuess.length !== ANSWER_LENGTH) {
         return;
     }
 
-    // TO DO:
-    // Validate word
-    // Add markers
-    // Winner or lose alerts
+    guessArray = currentGuess.split("");
 
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+        // Find the index of the first occurrence of the letter in wordArray
+        const firstIndex = wordArray.indexOf(guessArray[i]);
+        // Check if the letter exists in wordArray
+        if (firstIndex !== -1) {
+          if (guessArray[i] === wordArray[i]) {
+            letters[ANSWER_LENGTH * currentRow + i].classList.add('correct');
+          } else if (wordArray.includes(guessArray[i])) {
+            letters[firstIndex -1].classList.add('close');
+            console.log("this");
+          } else {
+            // Do nothing
+          }
+        }
+      }
 
     currentRow++;
     currentGuess = '';
@@ -63,4 +81,11 @@ const addLetter = (letter) => {
     letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
 }
 
+const setLoading = (isLoading) => {
+    setTimeout(() => {
+        loadingDiv.classList.toggle('show', isLoading);
+    },1000);
+}
+
+//Call the init async function
 init();
