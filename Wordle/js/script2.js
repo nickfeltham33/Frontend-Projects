df0c0682e88ab9f7158a4906d87eca5550ecc38e
nickfeltham33 <74492655+nickfeltham33@users.noreply.github.com>
2023-09-30 01:04:30 +0100

    validation and confetti + animations

--- Wordle/js/script2.js
+++ Wordle/js/script2.js
@@ -11,7 +11,6 @@ let currentRow = 0;
 let finished = false;
 
 
-
 async function init() {
     // Fetch the word of the day from the API
     const res = await fetch("https://words.dev-apis.com/word-of-the-day");
@@ -20,7 +19,7 @@ async function init() {
     // Extract and format the word to uppercase
     const word = resObj.word.toUpperCase();
     const wordArray = word.split(""); // Split the word into an array of letters
-
+    console.log(word);
     let isLoading = false;
     setLoading(isLoading); // Hide the loading spinner
 
@@ -36,7 +35,7 @@ async function init() {
 
         // Handle different key actions
         if (action === 'Enter') {
-            commit(wordArray, word); // Check the guess if Enter is pressed
+            commit(wordArray, word, setLoading); // Check the guess if Enter is pressed
         } else if (action === 'Backspace') {
             backspace(); // Handle backspace to remove letters
         } else if (isLetter(action)) {
@@ -53,12 +52,31 @@ const isLetter = (letter) => {
     return /^[a-zA-Z]$/.test(letter);
 }
 
-const commit = (wordArray, word) => {
+
+const commit = async (wordArray, word, setLoading) => {
     // Check if the currentGuess length is not equal to the desired ANSWER_LENGTH
     if (currentGuess.length !== ANSWER_LENGTH) {
         return;
     }
 
+    isLoading = true;
+    setLoading(true);
+
+    const res = await fetch("https://words.dev-apis.com/validate-word", {
+    method: "POST",
+    body: JSON.stringify({word: currentGuess})
+    });
+
+    const resObj = await res.json();
+    const { validWord } = resObj;
+
+    isLoading = false;
+    setLoading(false);
+
+    if (!validWord) {
+        invalidWord();
+        return;
+    }
     // Split the currentGuess into an array of individual letters
     guessArray = currentGuess.split("");
 
@@ -80,6 +98,7 @@ const commit = (wordArray, word) => {
             isCorrect = false; // The guess is not entirely correct
         }
     }
+
 
     // Loop through each letter in the guessArray again
     for (let i = 0; i < ANSWER_LENGTH; i++) {
@@ -113,6 +132,7 @@ const commit = (wordArray, word) => {
     }
 }
 
+
 const backspace = () => {
     // Remove the last character from the currentGuess string
     currentGuess = currentGuess.substring(0, currentGuess.length - 1);
@@ -120,6 +140,7 @@ const backspace = () => {
     // Clear the corresponding UI element in the letter grid
     letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
 }
+
 
 const addLetter = (letter) => {
     // Check if the current guess length is less than the ANSWER_LENGTH
@@ -141,6 +162,7 @@ const setLoading = (isLoading) => {
     loadingDiv.classList.toggle('show', isLoading);
 }
 
+
 const makeMap = (array) => {
     // object to store letters and values
     const obj = {};
@@ -161,9 +183,34 @@ const makeMap = (array) => {
     return obj;
 }
 
+const invalidWord = () => {
+    messageDiv.innerText = "That's not a real word, dude!";
+    
+    // Apply the "fade" class to trigger the fade-out effect
+    messageDiv.classList.add("fade");
+    
+    // Wait for a short delay, then remove the "fade" class and clear the message
+    setTimeout(() => {
+        messageDiv.classList.remove("fade");
+        messageDiv.innerText = "";
+    }, 3000); // Adjust the timeout duration as needed
+
+    // Apply the "invalid" class to the letter elements
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("invalid");
+        
+        // Remove the "invalid" class after a short delay
+        setTimeout(() => {
+            letters[currentRow * ANSWER_LENGTH + i].classList.remove("invalid");
+        }, 500); // Adjust the timeout duration as needed
+    }
+}
+
+
 const winner = () => {
     messageDiv.innerText = "YOU'RE A WINNER!!";
     reloadBtn.classList.add("show");
+    messageDiv.classList.add("winner-text");
     finished = true;
     const start = () => {
         confetti.start();
@@ -185,5 +232,6 @@ const loser = (word) => {
     reloadBtn.classList.add("show");
     finished = true;
 }
+
 //Call the init async function
 init();
